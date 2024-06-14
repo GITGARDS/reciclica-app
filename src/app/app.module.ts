99021d2b9ec5bce2de3593919565a41f7fe287bf
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-06-14 10:32:05 -0300

    Ionic Tutorial #10 - Starting with NGRX - Loading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppStoreModule } from 'src/store/AppStoreModule';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule] ,
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ...AppStoreModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    ComponentsModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ErrorMessageComponent } from './error-message/error-message.component';
+import { LoadingComponent } from './loading/loading.component';
 import { PickupCallCardComponent } from './pickup-call-card/pickup-call-card.component';
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  exports: [PickupCallCardComponent, ErrorMessageComponent],
-  declarations: [PickupCallCardComponent, ErrorMessageComponent],
+  declarations: [
+    PickupCallCardComponent,
+    ErrorMessageComponent,
+    LoadingComponent,
+  ],
+  exports: [PickupCallCardComponent, ErrorMessageComponent, LoadingComponent],
 })
 export class ComponentsModule {}
